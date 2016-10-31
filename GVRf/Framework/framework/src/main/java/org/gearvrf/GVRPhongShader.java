3e99227ed4d2dab7eb23c4b079a6ba56277521b2
Abhijit Jagdale <a.jagdale@partner.samsung.com>
2016-10-31 11:31:10 -0700

    Adding opacity to Phong shader

--- GVRf/Framework/framework/src/main/java/org/gearvrf/GVRPhongShader.java
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/GVRPhongShader.java
@@ -39,7 +39,7 @@ import org.gearvrf.R;
 
        public GVRPhongShader(GVRContext gvrcontext)
        {
-           super("float4 ambient_color; float4 diffuse_color; float4 specular_color; float4 emissive_color; float specular_exponent; float line_width",
+           super("float4 ambient_color; float4 diffuse_color; float4 specular_color; float4 emissive_color; float specular_exponent; float line_width; float u_opacity",
                  "sampler2D ambientTexture; sampler2D diffuseTexture; sampler2D specularTexture; sampler2D opacityTexture; sampler2D lightmapTexture; sampler2D emissiveTexture",
                  "float3 a_position float3 a_normal float2 a_texcoord float4 a_bone_weights int4 a_bone_indices float4 a_tangent float4 a_bitangent",
                    300);
@@ -81,6 +81,7 @@ import org.gearvrf.R;
            material.setVec4("emissive_color", 0.0f, 0.0f, 0.0f, 1.0f);
            material.setFloat("specular_exponent", 0.0f);
            material.setFloat("line_width", 1.0f);
+           material.setFloat("u_opacity", 0.0f);
        }
    }
 
