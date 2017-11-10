da4b0c8fe6cc2c217b1de82e0407102b355be979
Abhijit <a.jagdale@samsung.com>
2017-11-10 12:30:39 -0800

    Reverting back to static_cast

--- GVRf/Framework/framework/src/main/jni/vulkan/vk_render_texture.cpp
+++ GVRf/Framework/framework/src/main/jni/vulkan/vk_render_texture.cpp
@@ -45,7 +45,7 @@ const VkDescriptorImageInfo& VkRenderTexture::getDescriptorImage(){
 }
 
 void VkRenderTexture::createRenderPass(){
-    VulkanRenderer* vk_renderer= reinterpret_cast<VulkanRenderer*>(Renderer::getInstance());
+    VulkanRenderer* vk_renderer= static_cast<VulkanRenderer*>(Renderer::getInstance());
     VkRenderPass renderPass = vk_renderer->getCore()->createVkRenderPass(NORMAL_RENDERPASS, mSampleCount);
     clear_values.resize(3);
     fbo->addRenderPass(renderPass);
